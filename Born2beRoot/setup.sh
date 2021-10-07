6a2115c6e411fb396b624bd3f95d19f8344394b3
chegashi <mochegri@students.1337.ma>
2021-10-07 23:01:24 +0100

    add setup

--- Born2beRoot/setup.sh
+++ Born2beRoot/setup.sh
@@ -5,14 +5,15 @@ apt-get upgrade
 apt-get install vim
 apt-get install sudo
 addgroup sudo 
+addgroup user42
 usermod -a -G user42 mochegri
 usermod -a -G sudo mochegri
 reboot
 apt update
-mkdir /var/log/sudo
 
 #sudo
 
+mkdir /var/log/sudo
 echo 'Defaults	logfile="/var/log/sudo/sudo.log"' >> /etc/sudoers.d/rules
 echo 'Defaults	passwd_tries=3' >> /etc/sudoers.d/rules
 echo 'Defaults	badpass_message="Password is wrong, please try again"' >> /etc/sudoers.d/rules
@@ -33,14 +34,18 @@ awk '{ if (NR == 34) print "PermitRootLogin no"; else print $0}' tmp > /etc/ssh/
 apt install ufw
 ufw enable
 ufw allow 4242
+
+# password
 awk '{ if (NR == 160) print "PASS_MAX_DAYS   30"; else print $0}' /etc/login.defs > tmp
 awk '{ if (NR == 161) print "PASS_MIN_DAYS   2"; else print $0}' tmp > tmp2
 awk '{ if (NR == 162) print "PASS_MIN_DAYS   2"; else print $0}' tmp2 > /etc/login.defs
 
-# password
-
 apt install libpam-pwquality
 
-awk '{ if (NR == 25) print "password        requisite                       pam_pwquality.so retry=3 minlen=10 ucredit=-1 dcredit=-1 maxrepeat=3 reject_username difok=7 enforce_for_root"; else print $0}' tmp2 > /etc/login.defs
-	scripte monitoring:
+awk '{ if (NR == 25) print "password        requisite                       pam_pwquality.so retry=3 minlen=10 ucredit=-1 dcredit=-1 maxrepeat=3 reject_username difok=7 enforce_for_root"; else print $0}' /etc/pam.d/common-password > tmp
+mv tmp /etc/pam.d/common-password
+
+# scripte monitoring:
+# crontab -u root -e
+# line23 # m h  dom mon dow   command
 
---
+++ bonus.sh
@@ -0,0 +1,9 @@
+# apt-get -y update
+# apt-get -y upgrade
+# apt install -y wget
+
+# mysql < init.sql
+# apt install -y php-fpm php-mysql php-curl php-gd php-intl php-mbstring php-soap php-xml php-xmlrpc php-zip
+
+# service php7.3-fpm start
+# service  php7.3-fpm restart
